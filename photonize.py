82efe7ab3d64edc7aeac1bd2b5ece775005f23ea
aj <ali.becerra1224@gmail.com>
2023-07-08 22:46:37 -0700

    can get date of image from exif

--- photonize.py
+++ photonize.py
@@ -2,14 +2,33 @@ import os
 from PIL import Image
 from PIL.ExifTags import TAGS
 
+def get_date(img_source):
+    try:
+        exif_data = img_source._getexif()
+        
+        # Iterate over all Exif tags
+        for tag, value in exif_data.items():
+            tag_name = TAGS.get(tag)
+            if tag_name == 'DateTimeOriginal':
+                return value
+                
+    except (AttributeError, KeyError, IndexError):
+        # Handle cases where the image has no Exif data or no DateTimeOriginal tag
+        pass
+    
+    return None
+
+
+
+
 current_cwd = os.getcwd()
-print(current_cwd)
 file_list = os.listdir(current_cwd)
-print(file_list)
 
 file_ext = ["jpg", "jpeg", "JPG", "png", "PNG", "NEF", "THM", "AVI", "mp4"]
-
 cut_filelist = [n for n in file_list if n[-3:] in file_ext]
 
 print(cut_filelist)
 
+for image in cut_filelist:
+    current_image = Image.open(image)
+    print(get_date(current_image))
