bc4ca700546bddce0c21a9cbcdbdf89110fa5781
aj <ali.becerra1224@gmail.com>
2023-07-23 12:49:58 -0700

    fin organizer

--- photonize.py
+++ photonize.py
@@ -7,8 +7,6 @@ file_list = os.listdir(current_cwd)
 file_ext = ["jpg", "jpeg", "JPG", "png", "PNG", "NEF", "THM", "AVI", "mp4", "MOD"]
 cut_filelist = [n for n in file_list if n[-3:] in file_ext]
 
-# print(file_list)
-
 for source in cut_filelist:
     
     os_time = os.path.getmtime(source)
@@ -16,14 +14,10 @@ for source in cut_filelist:
     iso_time = modification_time.date().isoformat()
     
     dir_with_time = current_cwd + "/" + iso_time
-   
     dir_exist = os.path.exists(dir_with_time) 
-    
 
     if not dir_exist:
         os.mkdir(dir_with_time)
 
     os.rename(current_cwd + "/" + source, dir_with_time + "/" + source)
-    # print(source, iso_time)
         
-
